ee14abf1131e1189bb3806db6ad2284846a92a79
kazuki-n <>
2018-11-10 15:32:43 +0900

    [modify] add elements

--- src/pages/Random.vue
+++ src/pages/Random.vue
@@ -1,19 +1,51 @@
 <template lang="html">
   <div id="Random">
     <global-header></global-header>
-    <p>Random</p>
+
+    <!-- genre select box -->
+    <b-form-select id="genreInput3"
+                   :options="genre"
+                   required
+                   v-model="form.food">
+    </b-form-select>
+
+    <!-- talk level -->
+    <p>Value: {{ text1 }}</p>
+
+    <!-- range -->
+    <b-form-input id="levelInput3"
+                  v-model="text1"
+                  type="range"
+                  placeholder="Enter your name"></b-form-input>
+    <!-- talk button -->
+    <b-button size="lg" variant="primary">
+      Let's talk
+    </b-button>
+
   </div>
 </template>
 
 <script>
-import GlobalHeader from '../components/Header.vue'
+  import GlobalHeader from '../components/Header.vue'
 
-export default {
-  name: 'Random',
-  components: {
-    globalHeader: GlobalHeader
+  export default {
+    data() {
+      return {
+        form: {
+          food: null
+        },
+        genre: [
+          {text: 'Select a genre', value: null},
+          'life', 'love', 'work', 'other'
+        ],
+        text1: '50'
+      }
+    },
+    name: 'Random',
+    components: {
+      globalHeader: GlobalHeader
+    }
   }
-}
 </script>
 
 <style lang="less">
