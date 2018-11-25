dbe20f6b4cd86908076885be1723992b45e44a80
kazuki-n <>
2018-11-26 04:57:53 +0900

    [modify] add modal

---
+++ src/main/js/components/LetsTalkButton.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+  <div id="letsTalkButton">
+    <b-row align-h="center">
+      <!--トークボタン-->
+      <b-btn v-b-modal.letsTalkModal
+             size="lg"
+             class="m-3"
+             variant="primary">
+        Let's talk
+      </b-btn>
+    </b-row>
+    <!-- モーダルウィンドウ -->
+    <b-modal id="letsTalkModal"
+             hide-footer
+             centered
+             :title="this.talkTheme.title">
+      <category-tag :name="category.name"
+                    v-for="category in this.talkTheme.categoryList"
+                    :key="category.id"
+                    class="ml-1">
+      </category-tag>
+      <p class="ml-1">{{ this.talkTheme.description }}</p>
+      <b-row align-h="center">
+        <!--トーク詳細ボタン-->
+        <b-btn :to="this.talkThemeDetailUrl"
+               size="lg"
+               class="m-3"
+               variant="primary">
+          詳細へ
+        </b-btn>
+      </b-row>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import TalkThemeDto from '@/main/js/dto/TalkThemeDto'
+import CategoryTag from '@/main/js/components/CategoryTag.vue'
+// テストデータ（TODO:トークID？によって対象のトークテーマをAPIより取得する）
+const talkTheme1 = new TalkThemeDto(1, 1, 'タイトル1', 'this talk theme is ...', 'thumbnail_sample1.png', 100, 50, 200, [{name: 'tag1'}, {name: 'tag2'}], 'fugaさん', '201811111200')
+
+export default {
+  name: 'lets-talk-button',
+  components: {
+    categoryTag: CategoryTag
+  },
+  data () {
+    return {
+      talkTheme: talkTheme1,
+      talkThemeDetailUrl: ''
+    }
+  },
+  mounted () {
+    this.talkThemeDetailUrl = `/talk/${this.talkTheme.id}`
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/main/js/pages/Random.vue
+++ src/main/js/pages/Random.vue
@@ -1,79 +1,38 @@
 <template lang="html">
   <div id="Random">
     <global-header></global-header>
-
-    <!-- genre select box -->
-    <div id="genreSelect"
-         class="text-center">
-      <b-form-select id="genreInput"
-                     class="w-50 m-3"
-                     :options="genre"
-                     required
-                     v-model="form.food"
-                     v-b-popover.hover.right="'Select a Genre!'">
-      </b-form-select>
-    </div>
-
-    <!-- talk level -->
-    <div id="talkLevel"
-         class="text-center">
-      <p class="m-3">
-        {{ levels[text1] }}
-      </p>
-    </div>
-
-    <!-- range -->
-    <div id="levelRange"
-         class="text-center">
-      <label class="w-50">
-        <input class="w-100"
-               v-model="text1"
-               min="0" max="4"
-               v-b-popover.hover.right="'Select Level!'"
-               type="range"/>
-      </label>
-    </div>
-
-    <!-- talk button -->
-    <div id="talkButton"
-         class="text-center">
-      <b-button size="lg"
-                class="m-3"
-                variant="primary"
-                v-b-popover.hover.right="'Start Talking!'">
-        Let's talk
-      </b-button>
-    </div>
-
+    <random-talk-panel></random-talk-panel>
+    <post-talk></post-talk>
   </div>
 </template>
 
 <script>
 import GlobalHeader from '@/main/js/components/Header.vue'
+import PostTalk from '@/main/js/components/PostTalk.vue'
+import RandomTalkPanel from '@/main/js/components/RandomTalkPanel.vue'
+
+// TODO:マスタ化
+const levels = ['下ネタ', 'カジュアル', '普通', 'フォーマル', '知的']
 
 export default {
-  data () {
-    return {
-      form: {
-        food: null
-      },
-      // TODO:マスタ化
-      genre: [
-        {text: 'Select a genre', value: null},
-        'life', 'love', 'work', 'other'
-      ],
-      // TODO:マスタ化
-      levels: ['下ネタ', 'カジュアル', '普通', 'フォーマル', '知的'],
-      text1: '2'
-    }
-  },
   name: 'Random',
   components: {
-    globalHeader: GlobalHeader
+    globalHeader: GlobalHeader,
+    postTalk: PostTalk,
+    randomTalkPanel: RandomTalkPanel
+  },
+  data () {
+    return {
+      talkLevelValue: 400
+    }
+  },
+  computed: {
+    talkLevel () {
+      return levels[Math.round(this.talkLevelValue / 200)]
+    }
   }
 }
 </script>
 
-<style lang="less">
-  /* temp css */
+<style lang="less" scoped>
 </style>
